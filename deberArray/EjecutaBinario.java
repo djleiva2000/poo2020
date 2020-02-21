8c93df3189b7f2e305d76748a3b122dcbf07baf9
djleiva2000 <diegoleiva140@gmail.com>
2020-02-21 01:11:38 -0500

    deber de arreglos

---
+++ deberArray/EjecutaBinario.java
@@ -0,0 +1,15 @@
+package deberArray;
+import java.util.Scanner;
+
+public class EjecutaBinario {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingresar el número binario de 6 cifras");
+        String numBinario = entrada.nextLine();
+        Binario binario = new Binario(numBinario);
+        binario.calcularDecimal();
+        binario.obtenerDecimal();
+    }
+
+}
---
+++ deberArray/EjecutaDatos.java
@@ -0,0 +1,26 @@
+package deberArray;
+import java.util.Scanner;
+
+public class EjecutaDatos {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        String[] nom = new String[5];
+        int[] edad = new int[5];
+        String[] universidad = new String[5];
+        String[] cel = new String[5];
+        for (int i=0; i<nom.length; i++){
+            System.out.println("Ingresar nombre del estudiante "+(i+1));
+            nom[i] = teclado.nextLine();
+            System.out.println("Ingresar edad del estudiante "+(i+1));
+            edad [i]= teclado.nextInt();
+            teclado.nextLine();
+            System.out.println("Ingresar universidad del estudiante "+(i+1));
+            universidad[i] = teclado.nextLine();
+            System.out.println("Ingresar numero del celular del estudiante "+(i+1));
+            cel[i] = teclado.nextLine();
+        }
+        Datos datos = new Datos(nom, edad,universidad,cel);
+        datos.obtenerAlumno();
+
+    }
+}
