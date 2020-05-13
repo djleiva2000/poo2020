f14dffd7b0fd89278aa3910d0b771aa4c717791f
djleiva2000 <diegoleiva140@gmail.com>
2020-05-12 21:56:42 -0500

    deber

---
+++ Copia de Prototipado_XDDD/Ejercicio_Mayo_7.java
@@ -0,0 +1,26 @@
+package Prototipado_XDDD;
+import java.util.Scanner;
+
+public class Ejercicio_Mayo_7 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        int values[] = new int[10000000];
+        for (int i = 0; i < 10000000; i++) {
+
+
+        }
+        int index=-1;
+        for (int j = 0; j < values.length; j++) {
+            if (values[j]==80) {
+                index=j;
+                System.out.println(index=j);
+
+            }
+            System.out.println(index=j);
+
+
+        }
+    }
+
+}
